78095f16ffac8f35f75dc387c29d1062f04da7f4
anvincen <antvin100.av@gmail.com>
2025-04-23 15:38:49 +0200

    mandatory seems ok.

--- mandatory/verbose.sh
+++ mandatory/verbose.sh
@@ -14,13 +14,10 @@ echo "  ===  tree \e[4;32m/tmp/test\e[0m  ==="
 tree /tmp/test
 echo
 
-hexdump -C ../test/sample64 > dump_cl_sample
 echo "  === Run ./Famine ==="
 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --track-fds=yes -q ./Famine 
 # gdb Famine
 echo "Done."
-hexdump -C /tmp/test/sample64 > dump_sample
-hexdump -C Famine > ./dump_famine
 echo
 echo 'strings /tmp/test/sample64 | grep anvincen-eedy'
 strings /tmp/test/sample64 | grep anvincen-eedy
@@ -30,13 +27,13 @@ strings /tmp/test/infected | grep anvincen-eedy
 echo
 
 echo "  === Put clean executables in \e[4;34m/tmp/test\e[0m ==="
-# cp -v ../test/OK/ls ../test/OK/pwd /tmp/test/
 cp -v ../test/OK/ls /tmp/test/
+cp -v /bin/yes /tmp/test/
 echo
 
-echo "  === ./tmp/test/sample64 ===\e[0m"
-# valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --track-fds=yes -q /tmp/test/sample64
-gdb /tmp/test/sample64
+echo "  === /tmp/test/sample64 ===\e[0m"
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --track-fds=yes -q /tmp/test/sample64
+# gdb /tmp/test/sample64
 # /tmp/test/sample64
 echo
 echo 'strings /tmp/test/ls | grep anvincen-eedy'
@@ -47,4 +44,7 @@ strings /tmp/test/pwd | grep anvincen-eedy
 echo
 echo 'strings /tmp/test/infected | grep anvincen-eedy'
 strings /tmp/test/infected | grep anvincen-eedy
+echo
+echo 'strings /tmp/test/yes | grep anvincen-eedy'
+strings /tmp/test/yes | grep anvincen-eedy
 echo
